a65a282817e70af338ee8610c83eb24fd7df6884
CoderX <85693093+PlanetEditorX@users.noreply.github.com>
2025-03-23 11:10:38 +0800

    地址转换

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ from pathlib import Path
 root_folder = '' # 当前目录
 
 # 递归遍历目录树
-def deep_directory(path):
-    for item in path.rglob('*.md'):
-        # 分离文件名和后缀
-        path, _ext = os.path.splitext(item._raw_path)
-        markdown_text = read_file(path)
-        HTML2File(markdown_text, path)
+def deep_directory(path, _type='md'):
+    if _type == 'md':
+        for item in path.rglob('*.md'):
+            # 分离文件名和后缀
+            path, _ext = os.path.splitext(item._raw_path)
+            markdown_text = read_file(item._raw_path)
+            HTML2File(markdown_text, path)
+    else:
+        # 地址转换
+        for item in path.rglob('*.html'):
+            HTML_PATH(item)
 
 # 打开Markdown文件并读取内容
-def read_file(path, ext='.md'):
+def read_file(path):
     try:
-        with open(path + ext, 'r', encoding='utf-8') as md_file:
+        with open(path, 'r', encoding='utf-8') as md_file:
             return md_file.read()
     except Exception as e:
         print(f"▻ 读取{path}发生异常:", e)
@@ -29,22 +34,7 @@ def content_convert(text, path):
 
     # 替换md为html
     text = re.sub(f".md", ".html", text)
-
-    # 替换路径
-    # 使用 splitlines() 方法拆分换行符并去除空项
-    text_list = [item for item in text.splitlines() if item]
-    pattern = r"(assets/.*\.html)"
-    result_list = []
-    parent = Path(path).parent._raw_path
-    for item in text_list:
-        site = re.search(pattern, item)
-        if site:
-            result = re.sub(pattern, lambda match: parent + '\\' + match.group(1), item)
-            result_list.append(result.replace("/", "\\"))
-        else:
-            result_list.append(item)
-    result ="\n\n".join(result_list)
-    return result
+    return text
 
 # 将HTML保存到文件
 def HTML2File(text, path):
@@ -56,29 +46,21 @@ def HTML2File(text, path):
 # 地址转换
 def HTML_PATH(path):
     # 逐行读取文件
-    html = read_file(path, '.html')
-    print(html)
-    # 替换路径
-    # 使用 splitlines() 方法拆分换行符并去除空项
-    text_list = [item for item in text.splitlines() if item]
-    pattern = r"(assets/.*\.html)"
-    result_list = []
-    for item in text_list:
-        site = re.search(pattern, item)
-        if site:
-            result = re.sub(pattern, lambda match: path + '\\' + match.group(1), item)
-            result_list.append(result.replace("/", "\\"))
-        else:
-            result_list.append(item)
-    result ="\n\n".join(result_list)
-    # # 转换Markdown到HTML
-    # html_output = markdown.markdown(content_convert(text))
-    # with open(path+'.html', 'w', encoding='utf-8') as file:
-    #     file.write(html_output)
+    text = read_file(path._raw_path)
+    # 匹配所有的超链接
+    pattern = r'<a href="([^"]+)">'
+    matches = re.findall(pattern, text)
+    if matches:
+        for item in matches:
+            html_file = list(root_folder.glob(f"**/{item}"))[0]
+            text = text.replace(item, html_file._raw_path)
+    with open(path._raw_path, 'w', encoding='utf-8') as file:
+        file.write(text)
 
 
 if __name__ == "__main__":
     # 指定目录路径
     path = os.getcwd() + "\\test"
-    directory = Path(path)
-    deep_directory(directory)
+    root_folder = Path(path)
+    deep_directory(root_folder)
+    deep_directory(root_folder, 'html')
